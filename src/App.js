dd4518251bcd3a8155775dde959c290125bf36bc
huytran <huytran21007@gmail.com>
2022-10-03 17:05:06 +0700

    First push: Create boilerplate for react captstone movie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+/* import packages */
+import { Route, Routes } from "react-router-dom";
+
+/* import pages components */
+import Home from "./Pages/Home";
+import BookList from "./Pages/BookList";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/booklist"
+          element={
+            <h2 className="text-center text-red-400">Hello book reader</h2>
+          }
+        />
+      </Routes>
     </div>
   );
 }
