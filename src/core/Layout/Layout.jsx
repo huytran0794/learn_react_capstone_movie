51b72c75dae1dbae622c8db161ccd18c94faa116
huytran <huytran21007@gmail.com>
2022-10-11 03:35:48 +0700

    👌 IMPROVE: refactor react router by using outlet

--- src/core/Layout/Layout.jsx
+++ src/core/Layout/Layout.jsx
@@ -1,27 +1,23 @@
 /* import packages */
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 /* import global components */
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 
-export default function Layout({
-  HeaderComponent = Header,
-  BodyComponent,
-  FooterComponent = Footer,
-}) {
+export default function Layout() {
   let location = useLocation();
   return (
     <>
       {location.pathname !== "/login" && location.pathname !== "/register" && (
-        <HeaderComponent />
+        <Header />
       )}
       <main className="py-10">
-        <BodyComponent />
+        <Outlet />
       </main>
       {location.pathname !== "/login" && location.pathname !== "/register" && (
-        <FooterComponent />
+        <Footer />
       )}
     </>
   );
